0ece6acb646026d6d553ebf23a08451dae0e6445
Ashirbad <ashirbad.cs@gmail.com>
2023-11-24 11:59:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 
+/** Create A table */
+
+CREATE TABLE customer (
+    customerid INT AUTO_INCREMENT PRIMARY KEY,
+    customernumber INT NOT NULL UNIQUE CHECK (customernumber > 0),
+    lastname VARCHAR(30) NOT NULL,
+    firstname VARCHAR(30) NOT NULL,
+    areacode INT DEFAULT 71000,
+    address VARCHAR(50),
+    country VARCHAR(50) DEFAULT 'Malaysia'
+);
